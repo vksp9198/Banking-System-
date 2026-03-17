163d1de4b61048258bf6e6c0c95291cc010d8c1b
VikasMain_git91 <vp9198776636@gmail.com>
2026-03-17 17:28:38 +0530

    Add create account page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//sidebar starts
 const sidebar = document.querySelector('.sidebar');
 
 const showSidebar = document.querySelector('#showSidebar');
@@ -10,3 +11,18 @@ showSidebar.addEventListener('click', function (e) {
 hideSidebar.addEventListener('click', function (e) {
     sidebar.style.display = "none";
 })
+
+// create account page 
+let createAcc = document.querySelector('button').addEventListener('click', function () {
+    const name = document.querySelector('#name').value
+    const number = document.querySelector('#number').value
+    console.log(name)
+    console.log(number)
+    alert("Account created successful!");
+})
+
+
+//sidebar ends
+
+// Bank structure 
+
